6b76c5ff1bf03b3aa8f6f13498f60d96e78f90de
Geoffrey Grimm <ggrimm@rand.org>
2019-11-08 08:51:48 -0500

    pulled coverage into separate method and cleaned up

---
+++ optic/utils/coverage.R
@@ -0,0 +1,20 @@
+#' calculate percentage of time the 95 percent CI actually covers the true
+#' treatment effect estimate
+#' 
+#' @param beta vector of coefficients
+#' @param se vector of standard errors for coefficients provided in beta
+#' @param cf correction factor (single constant)
+#' @param te true treatment effect
+coverage <- function(beta,se,cf,te) {
+  if (length(beta) != length(se)) {
+    stop("vectors provided for 'beta' and 'se' must be equal length")
+  }
+  
+  se <- se * cf
+  #TODO: is iters the same as length as beta? beta and se need to be the same length?
+  ind <- rep(0,iters)
+  low95 <- beta - (1.96 * se)
+  high95 <- beta + (1.96 * se)
+  ind[te > low95 & te < high95] <- 1
+  return(sum(ind) / iters)
+}
