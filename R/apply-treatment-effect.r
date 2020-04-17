43499f09209a4838909f3f8af1dd50469b372c74
Geoffrey Grimm <ggrimm@rand.org>
2020-04-17 17:56:24 -0400

    changed var name in apply treatment, rewrote summary method to work with new output and condensed those methods together

--- R/apply-treatment-effect.r
+++ R/apply-treatment-effect.r
@@ -4,22 +4,22 @@
 #' 
 #' @param x data
 #' @param model_formula formula for sim run used to identify outcome
-#' @param te true effect as proportion of change (e.g., 0.05 = 5%)
+#' @param effect_magnitude effect as proportion of change (e.g., 0.05 = 5%)
 #' @param effect_direction "null", "pos", or "neg"
 #' @param concurrent bool for whether this is concurrent run or not
-apply_treatment_effect <- function(x, model_formula, te, effect_direction, concurrent) {
+apply_treatment_effect <- function(x, model_formula, effect_magnitude, effect_direction, concurrent) {
   outcome <- model_terms(model_formula)[["lhs"]]
   
   if (effect_direction == "null") {
     return(x)
   } else {
     if (effect_direction == "neg") {
-      te <- -1 * te
+      effect_magnitude <- -1 * effect_magnitude
     }
     if (concurrent) {
-      x[[outcome]] <- x[[outcome]] + (x[[outcome]] * te[1] * x[["treatment1"]]) + (x[[outcome]] * te[2] * x[["treatment2"]])
+      x[[outcome]] <- x[[outcome]] + (x[[outcome]] * effect_magnitude[1] * x[["treatment1"]]) + (x[[outcome]] * effect_magnitude[2] * x[["treatment2"]])
     } else {
-      x[[outcome]] <- x[[outcome]] + (x[[outcome]] * te * x[["treatment"]])
+      x[[outcome]] <- x[[outcome]] + (x[[outcome]] * effect_magnitude * x[["treatment"]])
     }
     return(x)
   }
